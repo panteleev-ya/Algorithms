7a0fb2a525926cfbfca4c1b4ff7e0918b8d5764c
Yaroslav Panteleev <31574114+SmartOven@users.noreply.github.com>
2020-03-27 14:48:26 +0300

    Update lab10_3.cpp

--- Lab_10/lab10_3.cpp
+++ Lab_10/lab10_3.cpp
@@ -1,36 +1,32 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-constexpr int INF = 10e5 + 7;
-#define int long long
+#define ll long long
 using std::cin;
 using std::cout;
 
-std::vector <int> p;
-int fp(int x);
-bool united(std::pair<int, int> e);
-void unite(std::pair<int, int> e);
+std::vector <ll> p;
+ll find(ll x);
+bool united(std::pair<ll, ll> e);
+void unite(std::pair<ll, ll> e);
 
-// Kruskal algo
-int32_t main() {
-	// freopen("spantree3.in", "r", stdin);
-	// freopen("spantree3.out", "w", stdout);
+int main() {
+	freopen("spantree3.in", "r", stdin);
+	freopen("spantree3.out", "w", stdout);
 
 	std::ios_base::sync_with_stdio(false);
-    int n, m, a, b, l;
+    ll n, m, a, b, l;
     cin >> n >> m;
     p.resize(n + 1);
-    std::vector <std::pair <int, std::pair <int, int>>> edges;
-    for (int i = 0; i < m; i++) {
+    std::vector <std::pair <ll, std::pair <ll, ll>>> edges;
+    for (size_t i = 0; i < m; i++) {
         cin >> a >> b >> l;
         edges.push_back({ l, {a, b} });
     }
-    int ans = 0;
+    ll ans = 0;
     sort(edges.begin(), edges.end());
-
-    for (int i = 1; i <= n; i++)
+    for (size_t i = 1; i <= n; i++)
         p[i] = i;
-
     for (auto& e : edges) {
         if (!united(e.second)) {
             unite(e.second);
@@ -41,18 +37,18 @@ int32_t main() {
 	return 0;
 }
 
-int fp(int x) {
+ll find(ll x) {
     if (p[x] == x) return x;
-    return p[x] = fp(p[x]);
+    return p[x] = find(p[x]);
 }
 
-bool united(std::pair<int, int> e) {
-    return fp(e.first) == fp(e.second);
+bool united(std::pair<ll, ll> e) {
+    return find(e.first) == find(e.second);
 }
 
-void unite(std::pair<int, int> e) {
-    e.first = fp(e.first);
-    e.second = fp(e.second);
+void unite(std::pair<ll, ll> e) {
+    e.first = find(e.first);
+    e.second = find(e.second);
     if (e.first != e.second)
         p[e.first] = e.second;
 }
